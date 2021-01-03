0c25bd95df1453d31af27333fa82792da55dd203
KishoreMuthyala <67886934+KishoreMuthyala@users.noreply.github.com>
2021-01-03 14:34:32 +0530

    admin login completed

--- backend/controllers/employeController.js
+++ backend/controllers/employeController.js
@@ -8,88 +8,115 @@ import gravatar from "gravatar";
 // desc     Adding employe
 const addEmploye = async (req, res) => {
   //res.send("Adding Employe...");
-  const {username,firstname,lastname,email,gender,dateofbirth,contactnumber,role,address,salary,bloodgroup,maritialstatus,nationality,department,worktype,joiningdate,team} = req.body;
-    
+  const {
+    username,
+    firstname,
+    lastname,
+    email,
+    gender,
+    dateofbirth,
+    contactnumber,
+    role,
+    address,
+    salary,
+    bloodgroup,
+    maritialstatus,
+    nationality,
+    department,
+    worktype,
+    joiningdate,
+    team,
+  } = req.body;
 
-    if(!username||!firstname||!lastname||!email||!gender||!dateofbirth||!contactnumber||!role||!address||!salary||!bloodgroup||!maritialstatus||!nationality||!department||!worktype||!joiningdate){
-        return res.status(400).json({err:"Please fill all fields"})
-    }
+  if (
+    !username ||
+    !firstname ||
+    !lastname ||
+    !email ||
+    !gender ||
+    !dateofbirth ||
+    !contactnumber ||
+    !role ||
+    !address ||
+    !salary ||
+    !bloodgroup ||
+    !maritialstatus ||
+    !nationality ||
+    !department ||
+    !worktype ||
+    !joiningdate
+  ) {
+    return res.status(400).json({ err: "Please fill all fields" });
+  }
 
-  let employeEmailExists = await Employe.findOne({email});
-  let employeUsernameExists = await Employe.findOne({username});
+  let employeEmailExists = await Employe.findOne({ email });
+  let employeUsernameExists = await Employe.findOne({ username });
 
-  if(employeEmailExists || employeUsernameExists){
-      return res.status(400).json({err:"Employee already exists"});
+  if (employeEmailExists || employeUsernameExists) {
+    return res.status(400).json({ err: "Employee already exists" });
   }
 
   let salt = await bcryptjs.genSalt(10);
-  let hashed_password = await bcryptjs.hash("stackhack",salt);
+  let hashed_password = await bcryptjs.hash("stackhack", salt);
 
-  const avatar = gravatar.url(email,{
-      s:"200",
-      r:"pg",
-      d:"mm",
-  })
+  const avatar = gravatar.url(email, {
+    s: "200",
+    r: "pg",
+    d: "mm",
+  });
 
   let username_id_string = username;
   //console.log(emp_id[0]);
-  let contact_number_first_three_digits = contactnumber.slice(0,3);
-  let employe_id = username_id_string+contact_number_first_three_digits;
-
+  let contact_number_first_three_digits = contactnumber.slice(0, 3);
+  let employe_id = username_id_string + contact_number_first_three_digits;
 
   const employe = await Employe.create({
-      username,
-      firstname,
-      lastname,
-      email,
-      gender,
-      dateofbirth,
-      contactnumber,
-      role,
-      address,
-      salary,
-      bloodgroup,
-      maritialstatus,
-      nationality,
-      department,
-      worktype,
-      joiningdate,
-      team,
-      password:hashed_password,
-      photo:avatar,
-      employeid:employe_id
-  })
+    username,
+    firstname,
+    lastname,
+    email,
+    gender,
+    dateofbirth,
+    contactnumber,
+    role,
+    address,
+    salary,
+    bloodgroup,
+    maritialstatus,
+    nationality,
+    department,
+    worktype,
+    joiningdate,
+    team,
+    password: hashed_password,
+    photo: avatar,
+    employeid: employe_id,
+  });
 
-  if(employe){
-      res.status(201).json({
-          _id:employe._id,
-          username:employe.username,
-          email:employe.email,
-          isadmin:employe.isadmin,
-          token:generateToken(employe._id),
-          employeid:employe.employeid
-      })
-  }else{
-      return res.status(400).json({
-          err:"Error to save employe"
-      })
+  if (employe) {
+    res.status(201).json({
+      _id: employe._id,
+      username: employe.username,
+      email: employe.email,
+      isadmin: employe.isadmin,
+      token: generateToken(employe._id),
+      employeid: employe.employeid,
+    });
+  } else {
+    return res.status(400).json({
+      err: "Error to save employe",
+    });
   }
 };
 
-const displayEmployee = async(req,res) => {
+const displayEmployee = async (req, res) => {
+  const all = await Employe.find();
 
-    const all = await Employe.find();
+  if (all) {
+    return res.json(all);
+  } else {
+    return res.status(400).json({ err: "Error to get all employees" });
+  }
+};
 
-    if(all){
-        return res.json({all});
-    }else{
-        return res.status(400).json({err:"Error to get all employees"})
-    }
-
-}
-
-
-
-
-export { addEmploye,displayEmployee };
-
+export { addEmploye, displayEmployee };
--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,61 +1,60 @@
 import jwt from "jsonwebtoken";
 import Employe from "../models/employeModel.js";
 
-const protect = async(req,res,next) =>{
-
-    let token;
-    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
-        try {
-            token = req.headers.authorization.split(" ")[1];
-            const decoded = jwt.verify(token,process.env.JWT_SECRET);
-           // console.log(decoded);
-            req.user = await Employe.findById(decoded.id).select("-password");
-            //console.log(req.headers);
-            //console.log(req.user)
-            next()  
-            
-        } catch (err) {
-            console.log(err.message);
-            return res.status(401).json({err:"Not Authorized, token failed"});
-        }
+const protect = async (req, res, next) => {
+  let token;
+  // && req.headers.authorization.startsWith("Bearer")  .split(" ")[1]
+  if (req.headers.authorization) {
+    try {
+      token = req.headers.authorization;
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // console.log(decoded);
+      req.user = await Employe.findById(decoded.id).select("-password");
+      //console.log(req.headers);
+      //console.log(req.user)
+      next();
+    } catch (err) {
+      console.log(err.message);
+      return res.status(401).json({ err: "Not Authorized, token failed" });
     }
+  }
 
-    if(!token){
-        //console.log(err.message);
-        return res.status(401).json({err:"Not Authorized, token failed"});
+  if (!token) {
+    //console.log(err.message);
+    return res.status(401).json({ err: "Not Authorized, token failed" });
+  }
+};
+
+const isAdminUser = async (req, res, next) => {
+  let token;
+  //&& req.headers.authorization.startsWith("Bearer") .split(" ")[1]
+  if (req.headers.authorization) {
+    try {
+      token = req.headers.authorization;
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // console.log(decoded);
+      req.user = await Employe.findById(decoded.id).select("-password");
+      //console.log(req.headers);
+      //console.log(req.user)
+      if (req.user.isadmin) {
+        next();
+      } else {
+        return res
+          .status(401)
+          .json({ err: "Not Authorized, You are not an admin" });
+      }
+    } catch (err) {
+      console.log(err.message);
+      return res
+        .status(401)
+        .json({ err: "Not Authorized, You are not an admin" });
     }
-}
+  }
 
-const isAdminUser = async(req,res,next) =>{
+  if (!token) {
+    //console.log(err.message);
+    return res.status(401).json({ err: "Not Authorized, token failed" });
+  }
+};
 
-    let token;
-    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
-        try {
-            token = req.headers.authorization.split(" ")[1];
-            const decoded = jwt.verify(token,process.env.JWT_SECRET);
-           // console.log(decoded);
-            req.user = await Employe.findById(decoded.id).select("-password");
-            //console.log(req.headers);
-            //console.log(req.user)
-            if(req.user.isadmin){
-                next();
-            }else{
-                return res.status(401).json({err:"Not Authorized, You are not an admin"});
-            }
-              
-            
-        } catch (err) {
-            console.log(err.message);
-            return res.status(401).json({err:"Not Authorized, You are not an admin"});
-        }
-    }
-
-    if(!token){
-        //console.log(err.message);
-        return res.status(401).json({err:"Not Authorized, token failed"});
-    }
-
-}
-
-
-export {protect,isAdminUser};
+export { protect, isAdminUser };
