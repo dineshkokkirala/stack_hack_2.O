a8d0b046abe6a6024667827702ba2402086b3540
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2021-01-05 17:04:14 +0530

    bug founded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import AuthState from "./context/auth/AuthState";
 import Admin from "./components/Admin";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import setAuthToken from "./utils/setAuthToken";
+import AddEmploye from "./components/AddEmploye";
+import Employee from "./components/Employee"
 
 if(localStorage.token){
   setAuthToken(localStorage.token)
